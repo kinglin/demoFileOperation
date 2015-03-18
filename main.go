1e508c9d0ab1c899b7086e73cd41af9f2ca75fb8
kinglin <lince110@163.com>
2015-03-18 14:21:25 +0800

    file/directory operation of create,open,remove,read,write

--- main.go
+++ main.go
@@ -2,9 +2,36 @@
 package main
 
 import (
-	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	//创建一个文件夹
+	//os.Mkdir("test", 0777)
+	//os.MkdirAll("C:\\filetest\\pic\\jpg", 0777)
+
+	//删除文件或文件夹
+	os.Remove("test\\test.txt")
+	os.RemoveAll("C:\\filetest")
+
+	//创建文件,写文件
+	fout, _ := os.Create("test\\test.txt")
+	for i := 0; i < 3; i++ {
+		fout.WriteString("it's a test\r\n")
+		fout.Write([]byte("this is a test\r\n"))
+	}
+
+	//将test中的内容复制到test1中
+	f1, _ := os.Open("test\\test.txt")
+	f2, _ := os.Create("test\\test1.txt")
+	defer f1.Close()
+	buf := make([]byte, 1024)
+	for {
+		n, _ := f1.Read(buf)
+		if 0 == n {
+			break
+		}
+		f2.Write(buf[:n])
+	}
+
 }
